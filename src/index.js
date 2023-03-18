f8d30aa17f4f0a69456e8fe782c76447c914844c
Shamil <Shamilino@gmail.com>
2023-03-18 16:59:58 +0300

    Создал redux toolkit и начну переводить проект

--- src/index.js
+++ src/index.js
@@ -3,12 +3,18 @@ import ReactDOM from 'react-dom/client';
 import './styles/index.scss';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
+import { store } from './app/store';
+import { Provider } from 'react-redux';
+
+console.log(store);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <Provider store={store}>
+        <App />
+      </Provider>
     </BrowserRouter>
   </React.StrictMode>,
 );
